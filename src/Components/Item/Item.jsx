e14934f46e3babf4cc4cacb54d44ed75d3960730
Abdalla Alhag <alhagabdalla@gmail.com>
2024-07-31 14:03:40 -0700

    Adjust hover on item component

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,21 +1,28 @@
-import { ItemContainer, HoverImg } from "./ItemStyled";
+import { ItemContainer } from "./ItemStyled";
 import { StyledImg, StyledP } from "../../style/CommonComponents";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 function Item({ item }) {
+  const [isHovered, setIsHovered] = useState(false);
 
   const completeImageUrl = item.imageUrl.startsWith("http")
     ? item.imageUrl
     : `https://${item.imageUrl}`;
 
   const completeHoverImageUrl = item.additionalImageUrls[0].startsWith("http")
-    ? item.hoverImageUrl
-    : `https://${item.hoverImageUrl}`;
+    ? item.additionalImageUrls[0]
+    : `https://${item.additionalImageUrls[0]}`;
 
+  console.log(completeHoverImageUrl);
   return (
     <ItemContainer>
-      <StyledImg src={completeImageUrl} alt={item.name} />
-      <HoverImg src={completeHoverImageUrl} alt={`${item.name} Hover`} />
+      <StyledImg
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        src={isHovered ? completeHoverImageUrl : completeImageUrl}
+        alt={item.name}
+      />
       <StyledP $textAlign="left" $fontSize="12px" $margin="10px 0px">
         {item.name}
       </StyledP>
--- src/Components/Item/ItemStyled.jsx
+++ src/Components/Item/ItemStyled.jsx
@@ -9,17 +9,3 @@ export const ItemContainer = styled(Column)`
   width: 320px; /* Adjust size as needed */
   height: auto;
 `;
-
-export const HoverImg = styled.img`
-  width: 100%;
-  height: auto;
-  transition: opacity 0.3s ease-in-out;
-  position: absolute;
-  top: 0;
-  left: 0;
-  opacity: 0;
-
-  ${ItemContainer}:hover & {
-    opacity: 1;
-  }
-`;
