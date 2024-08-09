1b0ebbb28a929c07ed3d85018d3a08efa56cd387
Abdalla Alhag <alhagabdalla@gmail.com>
2024-08-09 09:59:16 -0700

    Add abort controller in apiFecthWithCache to deal with race condition

--- src/API/useFetchDataWithCache.jsx
+++ src/API/useFetchDataWithCache.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import debounce from "lodash/debounce";
 
 const useFetchDataWithCache = (endpoint, options, debounceDelay = 500) => {
@@ -6,11 +6,15 @@ const useFetchDataWithCache = (endpoint, options, debounceDelay = 500) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const abortControllerRef = useRef(null);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const fetchData = useCallback(
     debounce(async (url, opts) => {
       setLoading(true);
       setError(null);
+
+      abortControllerRef.current?.abort();
+      abortControllerRef.current = new AbortController();
 
       const cachedData = localStorage.getItem(url);
       if (cachedData) {
@@ -20,11 +24,17 @@ const useFetchDataWithCache = (endpoint, options, debounceDelay = 500) => {
       }
 
       try {
-        const response = await fetch(url, opts);
+        const response = await fetch(url, opts, {
+          signal: abortControllerRef?.signal,
+        });
         const result = await response.json();
         localStorage.setItem(url, JSON.stringify(result));
         setData(result);
       } catch (err) {
+        if (error.name === "AbortError") {
+          console.log("Aborted");
+          return;
+        }
         setError(err);
       } finally {
         setLoading(false);
